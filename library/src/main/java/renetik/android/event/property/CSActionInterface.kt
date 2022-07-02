03c10be119eef26cf28279e37380909fc235e57a
Rene Dohan <dohan.rene@gmail.com>
2022-07-02 11:32:48 -0500

    Update CSActionInterface.kt

--- library/src/main/java/renetik/android/event/property/CSActionInterface.kt
+++ library/src/main/java/renetik/android/event/property/CSActionInterface.kt
@@ -3,8 +3,12 @@ package renetik.android.event.property
 import renetik.android.core.lang.property.isTrue
 import renetik.android.core.lang.property.setFalse
 import renetik.android.core.lang.property.setTrue
+import renetik.android.event.property.CSEventPropertyFunctions.property
 
 typealias CSActionInterface = CSEventProperty<Boolean>
+
 fun CSActionInterface.start() = setTrue()
 fun CSActionInterface.stop() = setFalse()
 val CSActionInterface.isRunning get() = isTrue
+fun action(function: ((Boolean) -> Unit)? = null)
+        : CSActionInterface = property(false, function)
