988874fa8050f9bd65127cc8865bcbbee5787f04
Rene Dohan <dohan.rene@gmail.com>
2022-09-04 17:49:09 +0200

    Update CSModel.kt

--- library/src/main/java/renetik/android/event/common/CSModel.kt
+++ library/src/main/java/renetik/android/event/common/CSModel.kt
@@ -3,6 +3,8 @@ package renetik.android.event.common
 
 import renetik.android.core.lang.CSAssociations
 import renetik.android.core.lang.CSLeakCanary.expectWeaklyReachable
+import renetik.android.core.logging.CSLog.logWarn
+import renetik.android.core.logging.CSLogMessage.Companion.message
 import renetik.android.event.CSEvent.Companion.event
 import renetik.android.event.fire
 import renetik.android.event.registration.CSRegistrations
@@ -21,6 +23,10 @@ open class CSModel(
     }
 
     override fun onDestroy() {
+        if (isDestroyed) {
+            logWarn { message("Already destroyed: $this") }
+            return
+        }
         isDestroyed = true
         registrations.cancel()
         eventDestroy.fire().clear()
