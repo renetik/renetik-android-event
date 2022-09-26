2f1a345208a37a3d946d87f408bc28b3a4e09724
Rene Dohan <dohan.rene@gmail.com>
2022-09-26 12:56:29 +0200

    refa

--- library/src/main/java/renetik/android/event/property/extension/MutableList+CSProperty.kt
+++ library/src/main/java/renetik/android/event/property/extension/MutableList+CSProperty.kt
@@ -7,9 +7,17 @@ import renetik.android.event.property.CSProperty
 import renetik.android.event.property.action
 import renetik.android.event.registration.CSRegistration
 
+@Deprecated("use fun <Item : CSHasDestroy> CSProperty<Int>.updated")
 fun <Item : CSHasDestroy> MutableList<Item>.updated(
     property: CSProperty<Int>, function: (index: Int) -> Item): CSRegistration =
     property.action { value ->
         size.update(value, onAdd = { index -> add(function(index)) },
             onRemove = { index -> removeAt(index).destroy() })
+    }
+
+fun <Item : CSHasDestroy> CSProperty<Int>.updating(
+    list: MutableList<Item>, function: (index: Int) -> Item): CSRegistration =
+    action { value ->
+        list.size.update(value, onAdd = { index -> list.add(function(index)) },
+            onRemove = { index -> list.removeAt(index).destroy() })
     }
