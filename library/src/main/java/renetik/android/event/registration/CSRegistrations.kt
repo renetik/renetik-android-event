83af0238a638c8c16f3dd0bf9dae846e253da086
Rene Dohan <dohan.rene@gmail.com>
2022-10-13 00:02:18 +0200

    ...

--- library/src/main/java/renetik/android/event/registration/CSRegistrations.kt
+++ library/src/main/java/renetik/android/event/registration/CSRegistrations.kt
@@ -3,26 +3,55 @@ package renetik.android.event.registration
 import androidx.annotation.AnyThread
 import renetik.android.core.java.lang.nanoTime
 import renetik.android.core.kotlin.collections.removeValue
+import renetik.android.core.lang.variable.CSVariable.Companion.variable
 import renetik.android.core.logging.CSLog.logWarn
 import renetik.android.core.logging.CSLogMessage.Companion.traceMessage
-import java.lang.System.nanoTime
 
-class CSRegistrations(val parent: Any) {
-    private val registrations: MutableMap<String, CSRegistration> = mutableMapOf()
-    var isCanceled = false
-
+class CSRegistrations(val parent: Any) : CSRegistration, CSHasRegistrations {
+    override val registrations: CSRegistrations = this
+    private val registrationMap: MutableMap<String, CSRegistration> = mutableMapOf()
+    override var isActive by variable(true, ::onActiveChange)
+    override var isCanceled = false
     private var idCount = 0
     private fun createUniqId() = "$idCount: $nanoTime".also { idCount++ }
 
+    private fun onActiveChange(isActive: Boolean) {
+        if (isCanceled) {
+            logWarn { traceMessage("Already canceled:$parent") }
+            return
+        }
+        registrationMap.forEach { it.value.setActive(isActive) }
+    }
+
     @Synchronized
     @AnyThread
-    fun cancel() {
+    override fun resume() {
+        if (isCanceled) {
+            logWarn { traceMessage("Already canceled:$this") }
+            return
+        }
+        if (isPaused) isActive = true else logWarn { traceMessage("Already resume:$this") }
+    }
+
+    @Synchronized
+    @AnyThread
+    override fun pause() {
+        if (isCanceled) {
+            logWarn { traceMessage("Already canceled:$this") }
+            return
+        }
+        if (isActive) isActive = false else logWarn { traceMessage("Already pause:$this") }
+    }
+
+    @Synchronized
+    @AnyThread
+    override fun cancel() {
         if (isCanceled) {
             logWarn { traceMessage("Already canceled:$parent") }
             return
         }
         isCanceled = true
-        registrations.onEach { it.value.cancel() }.clear()
+        registrationMap.onEach { it.value.cancel() }.clear()
     }
 
     @Synchronized
@@ -52,7 +81,7 @@ class CSRegistrations(val parent: Any) {
         if (isCanceled) logWarn { traceMessage("Already canceled:$parent") }
         if (registration.isCanceled) return registration
         if (isCanceled) return registration.also { it.cancel() }
-        registrations[key] = registration
+        registrationMap[key] = registration
         return registration
     }
 
@@ -63,18 +92,18 @@ class CSRegistrations(val parent: Any) {
         remove(key)
     }
 
-    private fun remove(key: String) = registrations.remove(key)?.cancel()
+    private fun remove(key: String) = registrationMap.remove(key)?.cancel()
 
     @Synchronized
     @AnyThread
     fun cancel(registration: CSRegistration) {
         if (registration.isCanceled) {
             logWarn { traceMessage("Registration already canceled:$registration") }
-            registrations.removeValue(registration)
+            registrationMap.removeValue(registration)
             return
         }
         registration.cancel()
-        if (!registrations.removeValue(registration)) logWarn {
+        if (!registrationMap.removeValue(registration)) logWarn {
             traceMessage("Registration not found")
         }
         if (isCanceled) {
@@ -83,15 +112,5 @@ class CSRegistrations(val parent: Any) {
         }
     }
 
-    @Synchronized
-    @AnyThread
-    fun setActive(active: Boolean) {
-        if (isCanceled) {
-            logWarn { traceMessage("Already canceled:$parent") }
-            return
-        }
-        registrations.forEach { it.value.setActive(active) }
-    }
-
-    val size: Int get() = registrations.size
+    val size: Int get() = registrationMap.size
 }
