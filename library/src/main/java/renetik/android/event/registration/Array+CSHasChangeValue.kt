d1560f36edd6023b7f8c13ea19fd0ccc9ed33780
Rene Dohan <dohan.rene@gmail.com>
2022-10-23 22:41:04 +0200

    ...

--- library/src/main/java/renetik/android/event/registration/Array+CSHasChangeValue.kt
+++ library/src/main/java/renetik/android/event/registration/Array+CSHasChangeValue.kt
@@ -4,9 +4,16 @@ import renetik.android.core.lang.Func
 import renetik.android.event.common.CSLaterOnceFunc.Companion.laterOnce
 import renetik.android.event.property.CSProperty
 
-fun <T : CSProperty<*>> Array<T>.onChangeLaterOnce(function: Func): CSRegistration {
+inline fun <T : CSProperty<*>> Array<T>.onChangeLater(
+    crossinline function: Func): CSRegistration {
     val registrations = CSRegistrationsList(this)
     val laterOnceFunction = registrations.laterOnce { function() }
     forEach { registrations.register(it.onChange { laterOnceFunction() }) }
     return registrations
+}
+
+inline fun <T : CSProperty<*>> Array<T>.actionOnChangeLater(
+    crossinline function: Func): CSRegistration {
+    function()
+    return onChangeLater(function)
 }
