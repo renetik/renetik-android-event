d211584540ab887b7e9cca036d07bf1595979800
Rene Dohan <dohan.rene@gmail.com>
2022-11-02 22:16:02 +0100

    ...

--- library/src/main/java/renetik/android/event/registration/CSHasRegistrations+Background.kt
+++ library/src/main/java/renetik/android/event/registration/CSHasRegistrations+Background.kt
@@ -1,11 +1,12 @@
 package renetik.android.event.registration
 
+import androidx.annotation.WorkerThread
 import renetik.android.core.lang.CSBackground
 import renetik.android.core.lang.Func
 import renetik.android.event.registration.CSRegistration.Companion.CSRegistration
 import java.util.concurrent.ScheduledFuture
 
-fun CSHasRegistrations.background(function: Func): CSRegistration {
+fun CSHasRegistrations.background(@WorkerThread function: Func): CSRegistration {
     var scheduled: ScheduledFuture<*>? = null
     val registration = register(CSRegistration(isActive = true,
         onCancel = { scheduled?.cancel(false) }))
