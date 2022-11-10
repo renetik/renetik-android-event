85d2f287930e4e121c97e6cb335cce68673858c8
Rene Dohan <dohan.rene@gmail.com>
2022-11-10 13:33:32 +0100

    ...

--- library/src/main/java/renetik/android/event/common/CSActionIsRunning.kt
+++ library/src/main/java/renetik/android/event/common/CSActionIsRunning.kt
@@ -2,11 +2,13 @@ package renetik.android.event.common
 
 
 import renetik.android.core.kotlin.collections.list
-import renetik.android.core.lang.variable.isTrue
 import renetik.android.event.property.CSActionInterface
 import renetik.android.event.property.CSProperty
 import renetik.android.event.property.CSProperty.Companion.property
 import renetik.android.event.property.CSPropertyWrapper
+import renetik.android.event.property.action
+import renetik.android.event.registration.CSRegistration
+import renetik.android.event.registration.CSRegistration.Companion.CSRegistration
 import renetik.android.event.registration.register
 
 class CSActionIsRunning(
@@ -20,14 +22,19 @@ class CSActionIsRunning(
         add(actions)
     }
 
-    fun add(actions: Array<out CSActionInterface>) = actions.forEach { add(it) }
+    private fun add(actions: Array<out CSActionInterface>) = actions.forEach { add(it) }
 
-    fun add(action: CSActionInterface) {
+    fun add(action: CSActionInterface): CSRegistration {
         fun updateIsRunning(isRunning: Boolean) {
             if (isRunning) runningActions.add(action) else runningActions.remove(action)
             property.value(runningActions.size > 0)
         }
-        updateIsRunning(action.isTrue)
-        register(action.onChange { isTrue -> updateIsRunning(isTrue) })
+
+        val actionOnChange = register(action.action { updateIsRunning(it) })
+
+        return CSRegistration(onCancel = {
+            actionOnChange.cancel()
+            runningActions.remove(action)
+        })
     }
 }
