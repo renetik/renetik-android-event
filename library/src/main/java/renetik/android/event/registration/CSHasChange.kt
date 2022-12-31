828e91fe5a377cdf43faa3d7a97d12a69ff9163c
Rene Dohan - Renetik <dohan.rene@gmail.com>
2022-12-31 14:50:52 +0400

    ...

--- library/src/main/java/renetik/android/event/registration/CSHasChange.kt
+++ library/src/main/java/renetik/android/event/registration/CSHasChange.kt
@@ -3,5 +3,14 @@ package renetik.android.event.registration
 import renetik.android.core.lang.void
 
 interface CSHasChange<Argument> {
+    companion object {
+        fun onChange(
+            vararg hasChanges: CSHasChange<*>, function: () -> void): CSRegistration {
+            val registrations = CSRegistrationsList(this)
+            hasChanges.forEach { registrations.register(it.onChange(function)) }
+            return registrations
+        }
+    }
+
     fun onChange(function: (Argument) -> void): CSRegistration
 }
