3f2d0dab5e4c80f4d9ba103151d03d233bac6819
Rene Dohan <dohan.rene@gmail.com>
2023-01-25 15:19:25 +0400

    ...

--- library/src/main/java/renetik/android/event/property/CSProperty.kt
+++ library/src/main/java/renetik/android/event/property/CSProperty.kt
@@ -10,23 +10,28 @@ interface CSProperty<T> : CSVariable<T>, CSHasChange<T>, CSHasChangeValue<T> {
 
     companion object {
         fun <T> property(
-            value: T, onChange: ((value: T) -> Unit)? = null): CSProperty<T> =
+            value: T, onChange: ((value: T) -> Unit)? = null
+        ): CSProperty<T> =
             CSPropertyImpl(value, onChange)
 
         fun <T> property(
-            onChange: ((value: T?) -> Unit)? = null): CSProperty<T?> =
+            onChange: ((value: T?) -> Unit)? = null
+        ): CSProperty<T?> =
             CSPropertyImpl(null, onChange)
 
         fun <T> lateProperty(
-            onChange: ((value: T) -> Unit)? = null): CSLateProperty<T> =
+            onChange: ((value: T) -> Unit)? = null
+        ): CSLateProperty<T> =
             CSLateProperty(onChange)
 
         fun <T> propertyDelegate(
-            property: CSProperty<T>): CSProperty<T> =
+            property: CSProperty<T>
+        ): CSProperty<T> =
             lateProperty<T>().apply { connect(property) }
 
         fun <T> nullableProperty(
-            onChange: ((value: T?) -> Unit)? = null): CSProperty<T?> =
+            onChange: ((value: T?) -> Unit)? = null
+        ): CSProperty<T?> =
             CSPropertyImpl(null, onChange)
     }
 }
