dd332087e49ee55a6c21e6cad5b82969c1316893
Rene Dohan <dohan.rene@gmail.com>
2023-02-04 10:41:04 +0400

    ...

--- library/src/main/java/renetik/android/event/property/CSValue+Int.kt
+++ library/src/main/java/renetik/android/event/property/CSValue+Int.kt
@@ -1,7 +1,17 @@
 package renetik.android.event.property
 
+import renetik.android.core.kotlin.primitives.update
+import renetik.android.core.lang.ArgFunc
 import renetik.android.core.lang.value.CSValue
+import renetik.android.event.common.CSHasDestruct
+import renetik.android.event.common.destruct
 
-operator fun CSValue<Int>.plus(value: Int): Int = this.value + value
+fun CSValue<Int>.update(
+    newCount: Int, onAdd: ArgFunc<Int>? = null, onRemove: ArgFunc<Int>? = null
+) = value.update(newCount, onAdd, onRemove)
 
-operator fun CSValue<Int>.compareTo(value: Int): Int = this.value.compareTo(value)
+fun <Item : CSHasDestruct> CSValue<Int>.update(
+    list: MutableList<Item>, function: (index: Int) -> Item
+) = list.size.update(value,
+    onAdd = { index -> list.add(function(index)) },
+    onRemove = { index -> list.removeAt(index).destruct() })
