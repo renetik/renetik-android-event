cba0b245516aa8877beb53936fb3e3d9c9e4afc0
Rene Dohan <dohan.rene@gmail.com>
2023-02-19 19:52:15 +0100

    ...

--- library/src/main/java/renetik/android/event/util/CSLater.kt
+++ library/src/main/java/renetik/android/event/util/CSLater.kt
@@ -2,7 +2,6 @@ package renetik.android.event.util
 
 import renetik.android.core.java.lang.CSThread.currentThread
 import renetik.android.core.java.lang.isMain
-import renetik.android.core.kotlin.unsupported
 import renetik.android.core.lang.CSMainHandler.postOnMain
 import renetik.android.core.lang.CSMainHandler.removePosted
 import renetik.android.event.registration.CSFunctionRegistration
@@ -10,10 +9,10 @@ import renetik.android.event.registration.CSRegistration
 
 object CSLater {
     fun later(delayMilliseconds: Int, function: () -> Unit): CSRegistration {
-        if (delayMilliseconds == 0) unsupported("delay have to be > 0")
         val registration = CSFunctionRegistration(function = { function() },
             onCancel = { removePosted(it.function) })
-        postOnMain(delayMilliseconds, registration.function)
+        val delay = if (delayMilliseconds < 5) 5 else delayMilliseconds
+        postOnMain(delay, registration.function)
         return registration
     }
 
