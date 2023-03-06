5fd503d12dd5b22f567a3fa3989564b8c2854992
Rene Dohan <dohan.rene@gmail.com>
2023-03-06 21:47:54 +0100

    ...

--- library/src/main/java/renetik/android/event/registration/CSHasChangeValue.kt
+++ library/src/main/java/renetik/android/event/registration/CSHasChangeValue.kt
@@ -1,5 +1,24 @@
 package renetik.android.event.registration
 
 import renetik.android.core.lang.value.CSValue
+import renetik.android.event.property.action
 
-interface CSHasChangeValue<T> : CSValue<T>, CSHasChange<T>
+interface CSHasChangeValue<T> : CSValue<T>, CSHasChange<T> {
+    companion object {
+        fun <T, V> onChange(
+            parent: CSHasChangeValue<T>,
+            child: (T) -> CSHasChangeValue<V>,
+            onChange: (V) -> Any
+        ): CSRegistration {
+            var childRegistration: CSRegistration? = null
+            val parentRegistration = parent.onChange {
+                childRegistration?.cancel()
+                childRegistration = child(parent.value).action { onChange(it) }
+            }
+            return CSRegistration.CSRegistration(isActive = true, onCancel = {
+                parentRegistration.cancel()
+                childRegistration?.cancel()
+            })
+        }
+    }
+}
