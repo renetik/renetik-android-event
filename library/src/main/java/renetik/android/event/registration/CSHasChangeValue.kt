e1a0329131dedf45403be3ae00d808ac398dee6c
Rene Dohan <dohan.rene@gmail.com>
2023-03-15 21:46:16 +0100

    ...

--- library/src/main/java/renetik/android/event/registration/CSHasChangeValue.kt
+++ library/src/main/java/renetik/android/event/registration/CSHasChangeValue.kt
@@ -5,15 +5,33 @@ import renetik.android.event.property.action
 
 interface CSHasChangeValue<T> : CSValue<T>, CSHasChange<T> {
     companion object {
-        fun <T, V> onChange(
-            parent: CSHasChangeValue<T>,
-            child: (T) -> CSHasChangeValue<V>,
-            onChange: (V) -> Any
+        inline fun <ParentValue, ChildValue> onChange(
+            parent: CSHasChangeValue<ParentValue>,
+            crossinline child: (ParentValue) -> CSHasChangeValue<ChildValue>,
+            crossinline onChange: (ChildValue) -> Unit
         ): CSRegistration {
             var childRegistration: CSRegistration? = null
             val parentRegistration = parent.onChange {
                 childRegistration?.cancel()
                 childRegistration = child(parent.value).action { onChange(it) }
+            }
+            return CSRegistration.CSRegistration(isActive = true, onCancel = {
+                parentRegistration.cancel()
+                childRegistration?.cancel()
+            })
+        }
+
+        inline fun <ParentValue, ChildValue> onChangeNullableChild(
+            parent: CSHasChangeValue<ParentValue>,
+            crossinline child: (ParentValue) -> CSHasChangeValue<ChildValue>?,
+            crossinline onChange: (ChildValue) -> Unit
+        ): CSRegistration {
+            var childRegistration: CSRegistration? = null
+            val parentRegistration = parent.onChange { parentValue ->
+                childRegistration?.cancel()
+                childRegistration = child(parentValue)?.action { childValue ->
+                    onChange(childValue)
+                }
             }
             return CSRegistration.CSRegistration(isActive = true, onCancel = {
                 parentRegistration.cancel()
