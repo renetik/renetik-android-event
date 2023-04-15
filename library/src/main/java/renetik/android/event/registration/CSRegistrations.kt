e74c9af83f1363380e25f1d4b6b711f0accbff25
Rene Dohan <dohan.rene@gmail.com>
2023-04-15 19:05:30 +0200

    ...

--- library/src/main/java/renetik/android/event/registration/CSRegistrations.kt
+++ library/src/main/java/renetik/android/event/registration/CSRegistrations.kt
@@ -1,7 +1,14 @@
 package renetik.android.event.registration
 
+import androidx.annotation.AnyThread
+
 interface CSRegistrations : CSRegistration {
+    @AnyThread
     fun register(registration: CSRegistration): CSRegistration
+
+    @AnyThread
     fun register(replace: CSRegistration?, registration: CSRegistration?): CSRegistration?
+
+    @AnyThread
     fun cancel(registration: CSRegistration)
 }
