67839304e3d90b66150c932324728a65f8ead5db
Rene Dohan <dohan.rene@gmail.com>
2023-05-05 21:31:16 +0200

    ...

--- library/src/main/java/renetik/android/event/common/CSService.kt
+++ library/src/main/java/renetik/android/event/common/CSService.kt
@@ -1,30 +1,23 @@
 package renetik.android.event.common
 
-
+import android.app.Service
 import renetik.android.core.kotlin.className
-import renetik.android.core.lang.CSAssociations
 import renetik.android.core.lang.CSLeakCanary.expectWeaklyReachable
-import renetik.android.core.logging.CSLog.logWarnTrace
+import renetik.android.core.logging.CSLog
+import renetik.android.event.CSEvent
 import renetik.android.event.CSEvent.Companion.event
 import renetik.android.event.fire
 import renetik.android.event.registration.CSRegistrationsMap
 
-open class CSModel(
-    parent: CSHasDestruct? = null) : CSHasRegistrationsHasDestruct {
-
-    val associated by lazy { CSAssociations() }
+abstract class CSService() : Service(), CSHasRegistrationsHasDestruct {
     final override val registrations by lazy { CSRegistrationsMap(this) }
     final override val eventDestruct by lazy { event<Unit>() }
     final override var isDestructed = false
         private set
 
-    init {
-        parent?.let { parent(it) }
-    }
-
     override fun onDestruct() {
         if (isDestructed) {
-            logWarnTrace { "Already destroyed: $this" }
+            CSLog.logWarnTrace { "Already destroyed: $this" }
             return
         }
         isDestructed = true
@@ -32,5 +25,9 @@ open class CSModel(
         eventDestruct.fire().clear()
         expectWeaklyReachable("$className $this onDestroy")
     }
-}
 
+    override fun onDestroy() {
+        super.onDestroy()
+        onDestruct()
+    }
+}
