de297fd60a8d1518f84a9ecd3e02cfe9c463f598
Rene Dohan <dohan.rene@gmail.com>
2023-07-13 22:20:37 +0200

    ...

--- library/src/main/java/renetik/android/event/property/CSSyncProperty.kt
+++ library/src/main/java/renetik/android/event/property/CSSyncProperty.kt
@@ -5,19 +5,16 @@ import renetik.android.event.common.CSHasDestruct
 
 interface CSSyncProperty<T> : CSSynchronizedVariable<T>, CSProperty<T>
 
-//TODO: Rename to safeProperty
 fun <T> CSHasDestruct.safeProperty(
     value: T, onChange: ((value: T) -> Unit)? = null
 ): CSSyncProperty<T> = CSSyncPropertyImpl(this, value, onChange)
 
-//TODO: Rename to safe
 fun <T> CSHasDestruct.safe(
     property: CSProperty<T>, onChange: ((value: T) -> Unit)? = null
 ): CSSyncProperty<T> = CSSyncPropertyImpl(
     this, property.value, onChange
 ).apply { connect(property) }
 
-//TODO: Rename to safe
 fun <T> CSProperty<T>.safe(
     parent: CSHasDestruct
 ): CSSyncProperty<T> = parent.safe(this)
