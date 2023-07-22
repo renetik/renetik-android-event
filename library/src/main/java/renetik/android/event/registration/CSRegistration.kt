77411c0062fbb71de6b42adcaa1a78cadfa19cf8
Rene Dohan <dohan.rene@gmail.com>
2023-07-23 00:26:08 +0200

    ...

--- library/src/main/java/renetik/android/event/registration/CSRegistration.kt
+++ library/src/main/java/renetik/android/event/registration/CSRegistration.kt
@@ -6,6 +6,7 @@ import renetik.android.core.lang.CSHasCancel
 import renetik.android.core.lang.Func
 import java.io.Closeable
 
+//TODO: Implement CSRegistrationMap and List with weak values wrappers..
 interface CSRegistration : CSHasCancel, Closeable {
     val isActive: Boolean
     val isCanceled: Boolean
