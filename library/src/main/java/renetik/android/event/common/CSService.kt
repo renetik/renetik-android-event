f7158084573a51b2593741e9ec3a468b8d2a9dcf
Rene Dohan <dohan.rene@gmail.com>
2023-08-01 14:00:21 +0200

    ...

--- library/src/main/java/renetik/android/event/common/CSService.kt
+++ library/src/main/java/renetik/android/event/common/CSService.kt
@@ -2,6 +2,7 @@ package renetik.android.event.common
 
 import android.app.Service
 import renetik.android.core.kotlin.className
+import renetik.android.core.kotlin.reflect.lazyValue
 import renetik.android.core.lang.CSLeakCanary.expectWeaklyReachable
 import renetik.android.core.logging.CSLog
 import renetik.android.event.CSEvent
@@ -9,7 +10,7 @@ import renetik.android.event.CSEvent.Companion.event
 import renetik.android.event.fire
 import renetik.android.event.registration.CSRegistrationsMap
 
-abstract class CSService() : Service(), CSHasRegistrationsHasDestruct {
+abstract class CSService : Service(), CSHasRegistrationsHasDestruct {
     final override val registrations by lazy { CSRegistrationsMap(this) }
     final override val eventDestruct by lazy { event<Unit>() }
     final override var isDestructed = false
@@ -21,8 +22,8 @@ abstract class CSService() : Service(), CSHasRegistrationsHasDestruct {
             return
         }
         isDestructed = true
-        registrations.cancel()
-        eventDestruct.fire().clear()
+        ::registrations.lazyValue?.cancel()
+        ::eventDestruct.lazyValue?.fire()?.clear()
         expectWeaklyReachable("$className $this onDestroy")
     }
 
