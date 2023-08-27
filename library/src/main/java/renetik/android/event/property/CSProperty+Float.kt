3aedfdd08dc08305c492f04bd989bc38c237640f
Rene Dohan <dohan.rene@gmail.com>
2023-08-27 14:40:22 +0200

    ...

--- library/src/main/java/renetik/android/event/property/CSProperty+Float.kt
+++ library/src/main/java/renetik/android/event/property/CSProperty+Float.kt
@@ -7,3 +7,7 @@ fun CSProperty<Float>.min(value: Float) = apply {
 fun CSProperty<Float>.max(value: Float) = apply {
     if (this.value > value) this.value(value, fire = false)
 }
+
+fun CSProperty<Float>.range(range: ClosedRange<Float>) = apply {
+    min(range.start).max(range.endInclusive)
+}
