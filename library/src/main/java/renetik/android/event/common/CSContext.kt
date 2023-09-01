082347b09796c5301f1791425021a2ec6cbc2ba5
Rene Dohan <dohan.rene@gmail.com>
2023-09-01 21:38:05 +0200

    update to ContextCompat.registerReceiver

--- library/src/main/java/renetik/android/event/common/CSContext.kt
+++ library/src/main/java/renetik/android/event/common/CSContext.kt
@@ -3,10 +3,13 @@ package renetik.android.event.common
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import renetik.android.core.extensions.content.register
+import renetik.android.core.extensions.content.unregister
 import renetik.android.core.lang.CSAssociations
 import renetik.android.core.lang.CSEnvironment.app
 import renetik.android.core.lang.CSLeakCanary.expectWeaklyReachable
-import renetik.android.core.lang.catchAllWarn
 import renetik.android.core.logging.CSLog.logWarnTrace
 import renetik.android.event.CSEvent.Companion.event
 import renetik.android.event.fire
@@ -47,6 +50,9 @@ abstract class CSContext : ContextWrapper, CSHasContext {
         expectWeaklyReachable("CSContext $this onDestroy")
     }
 
-    override fun unregisterReceiver(receiver: BroadcastReceiver) =
-        catchAllWarn { super.unregisterReceiver(receiver) }
+    override fun registerReceiver(
+        receiver: BroadcastReceiver?, filter: IntentFilter
+    ): Intent? = register(receiver, filter)
+
+    override fun unregisterReceiver(receiver: BroadcastReceiver) = unregister(receiver)
 }
