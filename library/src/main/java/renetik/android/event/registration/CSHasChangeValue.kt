583a2ec30f13db31e40564f074778ec4b929f5ed
Rene Dohan <dohan.rene@gmail.com>
2024-02-12 20:38:07 +0800

    ...

--- library/src/main/java/renetik/android/event/registration/CSHasChangeValue.kt
+++ library/src/main/java/renetik/android/event/registration/CSHasChangeValue.kt
@@ -10,19 +10,36 @@ interface CSHasChangeValue<T> : CSValue<T>, CSHasChange<T> {
             return onChange(function)
         }
 
+        inline fun <Argument1, Argument2> onChange(
+            item1: CSHasChangeValue<Argument1>,
+            item2: CSHasChangeValue<Argument2>,
+            crossinline onChange: (Argument1, Argument2) -> Unit,
+        ): CSRegistration = list(item1, item2).onChange {
+            onChange(item1.value, item2.value)
+        }
+
         inline fun <Argument1, Argument2> action(
             item1: CSHasChangeValue<Argument1>,
             item2: CSHasChangeValue<Argument2>,
-            crossinline onAction: (Argument1, Argument2) -> Unit
+            crossinline onAction: (Argument1, Argument2) -> Unit,
         ): CSRegistration = list(item1, item2).action {
             onAction(item1.value, item2.value)
+        }
+
+        inline fun <Argument1, Argument2, Argument3> onChange(
+            item1: CSHasChangeValue<Argument1>,
+            item2: CSHasChangeValue<Argument2>,
+            item3: CSHasChangeValue<Argument3>,
+            crossinline onChange: (Argument1, Argument2, Argument3) -> Unit,
+        ): CSRegistration = list(item1, item2, item3).onChange {
+            onChange(item1.value, item2.value, item3.value)
         }
 
         inline fun <Argument1, Argument2, Argument3> action(
             item1: CSHasChangeValue<Argument1>,
             item2: CSHasChangeValue<Argument2>,
             item3: CSHasChangeValue<Argument3>,
-            crossinline onAction: (Argument1, Argument2, Argument3) -> Unit
+            crossinline onAction: (Argument1, Argument2, Argument3) -> Unit,
         ): CSRegistration = list(item1, item2, item3).action {
             onAction(item1.value, item2.value, item3.value)
         }
