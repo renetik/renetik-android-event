d38b63d2ab5be6358ef7a8a4a4dc7c532d196a45
Rene Dohan <dohan.rene@gmail.com>
2024-02-21 13:51:09 +0800

    ...

--- library/src/main/java/renetik/android/event/registration/CSHasChangeValue.kt
+++ library/src/main/java/renetik/android/event/registration/CSHasChangeValue.kt
@@ -63,5 +63,15 @@ interface CSHasChangeValue<T> : CSValue<T>, CSHasChange<T> {
         ): CSRegistration = list(item1, item2, item3).action {
             onAction(item1.value, item2.value, item3.value)
         }
+
+        inline fun <Argument1, Argument2, Argument3, Argument4> action(
+            item1: CSHasChangeValue<Argument1>,
+            item2: CSHasChangeValue<Argument2>,
+            item3: CSHasChangeValue<Argument3>,
+            item4: CSHasChangeValue<Argument4>,
+            crossinline onAction: (Argument1, Argument2, Argument3, Argument4) -> Unit,
+        ): CSRegistration = list(item1, item2, item3, item4).action {
+            onAction(item1.value, item2.value, item3.value, item4.value)
+        }
     }
 }
