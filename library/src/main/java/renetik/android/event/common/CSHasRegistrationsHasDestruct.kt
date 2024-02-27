861678289aff833f984fcbe61f98fdfd317eebb4
Rene Dohan <dohan.rene@gmail.com>
2024-02-27 14:16:03 +0800

    crazy stuff

--- library/src/main/java/renetik/android/event/common/CSHasRegistrationsHasDestruct.kt
+++ library/src/main/java/renetik/android/event/common/CSHasRegistrationsHasDestruct.kt
@@ -1,5 +1,8 @@
 package renetik.android.event.common
 
+import renetik.android.core.lang.CSHasId
 import renetik.android.event.registration.CSHasRegistrations
 
 interface CSHasRegistrationsHasDestruct : CSHasRegistrations, CSHasDestruct
+
+interface CSHasRegistrationsHasDestructHasId : CSHasRegistrationsHasDestruct, CSHasId
