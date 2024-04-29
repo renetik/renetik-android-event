4d0731792ba479a09f9e613868d1c731af0d71b0
Rene Dohan <dohan.rene@email.com>
2024-04-29 18:01:50 +0800

    ...

--- library/src/main/java/renetik/android/event/common/CSHasRegistrationsHasDestruct.kt
+++ library/src/main/java/renetik/android/event/common/CSHasRegistrationsHasDestruct.kt
@@ -1,9 +1,5 @@
 package renetik.android.event.common
 
-import renetik.android.core.lang.CSHasId
 import renetik.android.event.registration.CSHasRegistrations
 
 interface CSHasRegistrationsHasDestruct : CSHasRegistrations, CSHasDestruct
-
-@Deprecated("Try to get rid of this.. ")
-interface CSHasRegistrationsHasDestructHasId : CSHasRegistrationsHasDestruct, CSHasId
