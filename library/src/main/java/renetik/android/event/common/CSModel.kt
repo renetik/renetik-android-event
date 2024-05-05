d877723a2dab5af613ebc9c9cefe7785c60cac67
Rene Dohan <dohan.rene@email.com>
2024-05-05 16:59:22 +0800

    ...

--- library/src/main/java/renetik/android/event/common/CSModel.kt
+++ library/src/main/java/renetik/android/event/common/CSModel.kt
@@ -3,6 +3,8 @@ package renetik.android.event.common
 import androidx.annotation.AnyThread
 import renetik.android.core.kotlin.className
 import renetik.android.core.lang.CSLeakCanary.expectWeaklyReachable
+import renetik.android.core.lang.atomic.CSAtomic
+import renetik.android.core.lang.atomic.CSAtomic.Companion.atomic
 import renetik.android.core.logging.CSLog.logWarnTrace
 import renetik.android.event.CSEvent.Companion.event
 import renetik.android.event.invoke
@@ -23,7 +25,7 @@ open class CSModel(
 
     final override val eventDestruct = event<Unit>()
 
-    final override var isDestructed = false
+    final override var isDestructed by atomic(false)
         private set
 
     init {
--- library/src/main/java/renetik/android/event/common/CSService.kt
+++ library/src/main/java/renetik/android/event/common/CSService.kt
@@ -5,9 +5,9 @@ import android.content.Intent
 import android.os.Binder
 import renetik.android.core.kotlin.className
 import renetik.android.core.lang.CSLeakCanary.expectWeaklyReachable
+import renetik.android.core.lang.atomic.CSAtomic.Companion.atomic
 import renetik.android.core.logging.CSLog.logWarnTrace
 import renetik.android.event.CSEvent.Companion.event
-import renetik.android.event.fire
 import renetik.android.event.invoke
 import renetik.android.event.registration.CSRegistrationsMap
 
@@ -22,7 +22,7 @@ abstract class CSService : Service(), CSHasRegistrationsHasDestruct {
     private val lazyRegistrations = lazy { CSRegistrationsMap(this) }
     final override val registrations by lazyRegistrations
     final override val eventDestruct = event<Unit>()
-    final override var isDestructed = false
+    final override var isDestructed by atomic(false)
         private set
 
     override fun onDestruct() {
