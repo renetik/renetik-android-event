81a0f2e64e47a67c87368cff0e37a36be0533706
Renetik <dohan.rene@gmail.com>
2024-07-10 15:52:33 +0800

    ...

--- library/src/main/java/renetik/android/event/property/CSSafePropertyImpl.kt
+++ library/src/main/java/renetik/android/event/property/CSSafePropertyImpl.kt
@@ -1,8 +1,8 @@
 package renetik.android.event.property
 
-import java.util.concurrent.atomic.AtomicReference
 import renetik.android.event.common.CSHasDestruct
 import renetik.android.event.util.CSLater.onMain
+import java.util.concurrent.atomic.AtomicReference
 
 class CSSafePropertyImpl<T>(
     parent: CSHasDestruct,
@@ -12,12 +12,17 @@ class CSSafePropertyImpl<T>(
     private val field = AtomicReference(value)
 
     override fun value(newValue: T, fire: Boolean) {
-        if (field.getAndSet(newValue) != newValue) onMain {
-            onValueChanged(newValue, fire)
-        }
+        if (field.getAndSet(newValue) != newValue)
+            onMain { onValueChanged(newValue, fire) }
     }
 
     override var value: T
         get() = this.field.get()
         set(value) = value(value)
+
+    companion object {
+        fun <T> CSHasDestruct.safeProperty(
+            value: T, onChange: ((value: T) -> Unit)? = null
+        ) = CSSafePropertyImpl(this, value, onChange)
+    }
 }
