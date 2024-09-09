657eb6d377affef35a60dbb8f768c0f21cc41536
Rene Dohan <dohan.rene@email.com>
2024-09-09 13:20:54 +0800

    ....

--- library/src/main/java/renetik/android/event/registration/CSHasRegistrations+MainHandler.kt
+++ library/src/main/java/renetik/android/event/registration/CSHasRegistrations+MainHandler.kt
@@ -2,8 +2,14 @@ package renetik.android.event.registration
 
 import androidx.annotation.AnyThread
 import renetik.android.core.lang.CSHandler.mainHandler
+import kotlin.time.Duration
 
 @AnyThread
 inline fun CSHasRegistrations.laterEach(
     after: Int, period: Int = after, crossinline function: () -> Unit,
+): CSRegistration = this + mainHandler.laterEach(after, period, function)
+
+@AnyThread
+inline fun CSHasRegistrations.laterEach(
+    after: Duration, period: Duration = after, crossinline function: () -> Unit,
 ): CSRegistration = this + mainHandler.laterEach(after, period, function)
