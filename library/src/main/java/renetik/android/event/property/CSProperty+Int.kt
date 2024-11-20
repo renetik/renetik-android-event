4827f82f6e97d09363ab3c8e26a87c821b5dfc48
Rene Dohan <dohan.rene@email.com>
2024-11-20 16:19:23 +0800

    ...

---
+++ library/src/main/java/renetik/android/event/property/CSProperty+Int.kt
@@ -0,0 +1,10 @@
+package renetik.android.event.property
+
+//dec should return new value so not valid for property
+//operator fun CSProperty<Int>.dec(): CSProperty<Int> = apply {
+//    value--
+//}
+//inc should return new value so not valid for property
+//operator fun CSProperty<Int>.inc(): CSProperty<Int> = apply {
+//    value++
+//}
