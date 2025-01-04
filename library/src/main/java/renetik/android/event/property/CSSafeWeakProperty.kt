06830d3fea41f86f42610badf56add8efad19be0
Rene Dohan <dohan.rene@email.com>
2025-01-04 21:00:07 +0800

    ...

--- library/src/main/java/renetik/android/event/property/CSSafeWeakProperty.kt
+++ library/src/main/java/renetik/android/event/property/CSSafeWeakProperty.kt
@@ -3,24 +3,33 @@ package renetik.android.event.property
 import renetik.android.event.common.CSHasDestruct
 import renetik.android.event.util.CSLater.onMain
 import java.lang.ref.WeakReference
+import java.util.concurrent.atomic.AtomicReference
 
 class CSSafeWeakProperty<T>(
     parent: CSHasDestruct,
-    value: T? = null, onChange: ((value: T?) -> Unit)? = null
+    value: T? = null,
+    onChange: ((value: T?) -> Unit)? = null
 ) : CSPropertyBase<T?>(parent, onChange), CSSafeProperty<T?> {
 
-    private var _value: WeakReference<T>? = null
+    private val _value = AtomicReference<WeakReference<T?>?>(null)
 
-    override fun value(newValue: T?, fire: Boolean): Unit = synchronized(this) {
-        if (_value?.get() == newValue) return
-        _value = WeakReference(newValue)
-        onMain { onValueChanged(newValue) }
+
+    override fun value(newValue: T?, fire: Boolean) {
+        //This was proposed by Gemini and ChaGpt after long conversation..
+        while (true) {
+            val current = _value.get()
+            if (current?.get() == newValue) return
+            val weakReference = WeakReference(newValue)
+            if (_value.compareAndSet(current, weakReference)) {
+                onMain { onValueChanged(newValue) }
+                return
+            }
+        }
     }
 
-    @get:Synchronized
     override var value: T?
-        get() = _value?.get()
-        set(value) = value(value)
+        get() = _value.get()?.get()
+        set(value) = this.value(value)
 
     init {
         value?.let { this.value = it }
@@ -28,7 +37,8 @@ class CSSafeWeakProperty<T>(
 
     companion object {
         fun <T> CSHasDestruct.safeWeakProperty(
-            value: T? = null, onChange: ((value: T?) -> Unit)? = null
+            value: T? = null,
+            onChange: ((value: T?) -> Unit)? = null
         ): CSProperty<T?> = CSSafeWeakProperty(this, value, onChange)
     }
 }
