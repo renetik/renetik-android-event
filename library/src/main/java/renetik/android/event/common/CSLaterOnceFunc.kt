5cf31905d3b53ceeb79b3bb1ba86d06351c53ae7
Rene Dohan <dohan.rene@email.com>
2025-01-25 21:00:08 +0800

    ...

--- library/src/main/java/renetik/android/event/common/CSLaterOnceFunc.kt
+++ library/src/main/java/renetik/android/event/common/CSLaterOnceFunc.kt
@@ -1,5 +1,7 @@
 package renetik.android.event.common
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
 import renetik.android.core.lang.CSFunc
 import renetik.android.event.registration.CSHasRegistrations
@@ -9,21 +11,33 @@ import kotlin.time.Duration
 
 class CSLaterOnceFunc(
     private val parent: CSHasRegistrations,
+    private val dispatcher: CoroutineDispatcher = Main,
     val function: suspend () -> Unit,
     private val after: Int = 0,
 ) : CSFunc {
 
     companion object {
-        fun CSHasRegistrations.laterOnceFunc(after: Duration, function: suspend () -> Unit) =
-            CSLaterOnceFunc(this, function, after.inWholeMilliseconds.toInt())
+        fun CSHasRegistrations.laterOnceFunc(
+            after: Duration, function: suspend () -> Unit) =
+            CSLaterOnceFunc(this, Main, function, after.inWholeMilliseconds.toInt())
 
-        fun CSHasRegistrations.laterOnceFunc(function: suspend () -> Unit) =
-            CSLaterOnceFunc(this, function)
+        fun CSHasRegistrations.laterOnceFunc(
+            dispatcher: CoroutineDispatcher = Main,
+            after: Duration, function: suspend () -> Unit) =
+            CSLaterOnceFunc(this, dispatcher, function, after.inWholeMilliseconds.toInt())
+
+        fun CSHasRegistrations.laterOnceFunc(
+            dispatcher: CoroutineDispatcher = Main, function: suspend () -> Unit) =
+            CSLaterOnceFunc(this, dispatcher, function)
+
+        fun CSHasRegistrations.laterOnceFunc(
+            function: suspend () -> Unit) =
+            CSLaterOnceFunc(this, Main, function)
     }
 
     var registration: CSRegistration? = null
     override operator fun invoke() {
         registration?.cancel()
-        registration = parent.launch { delay(after.toLong()); function() }
+        registration = parent.launch(dispatcher) { delay(after.toLong()); function() }
     }
 }
