e8936b4a39730a52e1edd8c78ede30dd707019bb
Rene Dohan <dohan.rene@email.com>
2025-01-26 00:47:19 +0800

    ...

--- library/src/main/java/renetik/android/event/property/CSAction.kt
+++ library/src/main/java/renetik/android/event/property/CSAction.kt
@@ -8,8 +8,8 @@ import renetik.android.event.property.CSProperty.Companion.property
 typealias CSActionInterface = CSProperty<Boolean>
 
 operator fun CSActionInterface.invoke() = setTrue()
-fun CSActionInterface.start() = setTrue()
-fun CSActionInterface.stop() = setFalse()
+fun CSActionInterface.start() {setTrue()}
+fun CSActionInterface.stop() {setFalse()}
 val CSActionInterface.isRunning get() = isTrue
 
 object CSAction {
