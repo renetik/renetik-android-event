af3f97845a97043894b1532ec24f42874f7215f5
Rene Dohan <dohan.rene@email.com>
2025-02-20 23:48:14 +0800

    Fix memory leak reports ?

--- library/src/main/java/renetik/android/event/common/Debouncer.kt
+++ library/src/main/java/renetik/android/event/common/Debouncer.kt
@@ -9,26 +9,32 @@ import kotlinx.coroutines.flow.collectLatest
 import renetik.android.core.lang.CSFunc
 import renetik.android.event.registration.CSHasRegistrations
 import renetik.android.event.registration.launch
+import renetik.android.event.registration.onCancel
 import kotlin.time.Duration
 
 class Debouncer(
     parent: CSHasRegistrations,
     dispatcher: CoroutineDispatcher = Dispatchers.Main,
-    private val action: suspend () -> Unit,
+    action: suspend () -> Unit,
     private val after: Duration = Duration.ZERO,
 ) : CSFunc {
+
+    @Volatile
+    var action: (suspend () -> Unit)? = action
+
     private val flow = MutableSharedFlow<Unit>(
         replay = 1, extraBufferCapacity = 0,
         onBufferOverflow = BufferOverflow.DROP_OLDEST
     )
 
     init {
-        parent.launch(dispatcher) {
+        val sss = parent.launch(dispatcher) {
             flow.collectLatest {
                 delay(after)
                 action()
             }
         }
+        sss.onCancel { this.action = null }
     }
 
     override operator fun invoke() {
