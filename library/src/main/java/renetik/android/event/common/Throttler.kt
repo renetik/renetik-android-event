75865654c1a6ccccee50dd474e6eaf5e43018919
Rene Dohan <dohan.rene@email.com>
2025-02-24 12:55:49 +0800

    ...

--- library/src/main/java/renetik/android/event/common/Throttler.kt
+++ library/src/main/java/renetik/android/event/common/Throttler.kt
@@ -2,6 +2,7 @@ package renetik.android.event.common
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -17,9 +18,28 @@ import kotlin.time.Duration
 class Throttler(
     parent: CSHasRegistrations,
     dispatcher: CoroutineDispatcher = Dispatchers.Main,
-    private val after: Duration = Duration.ZERO,
     private val action: suspend () -> Unit,
+    private val after: Duration = Duration.ZERO,
 ) : CSFunc {
+    companion object {
+        fun CSHasRegistrations.throttler(
+            after: Duration, function: suspend () -> Unit) =
+            Throttler(this, Main, function, after)
+
+        fun CSHasRegistrations.throttler(
+            dispatcher: CoroutineDispatcher = Main,
+            after: Duration, function: suspend () -> Unit) =
+            Throttler(this, dispatcher, function, after)
+
+        fun CSHasRegistrations.throttler(
+            dispatcher: CoroutineDispatcher = Main, function: suspend () -> Unit) =
+            Throttler(this, dispatcher, function)
+
+        fun CSHasRegistrations.throttler(
+            function: suspend () -> Unit) =
+            Throttler(this, Main, function)
+    }
+
     private val flow = MutableSharedFlow<Unit>(
         replay = 0, extraBufferCapacity = 1,
         onBufferOverflow = BufferOverflow.DROP_OLDEST
