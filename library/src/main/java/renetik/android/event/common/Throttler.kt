1c3a13a31957b037b408bea238b4a3a6a8f3044c
Rene Dohan <dohan.rene@email.com>
2025-03-03 20:02:30 +0800

    ...

--- library/src/main/java/renetik/android/event/common/Throttler.kt
+++ library/src/main/java/renetik/android/event/common/Throttler.kt
@@ -6,55 +6,121 @@ import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
-import renetik.android.core.lang.CSFunc
 import renetik.android.event.registration.CSHasRegistrations
 import renetik.android.event.registration.launch
 import kotlin.time.Duration
+
+///**
+// * 1. On the first call, [action] is executed after [after].
+// * 2. Any calls arriving while waiting are ignored.
+// */
+//class Throttler(
+//    parent: CSHasRegistrations,
+//    dispatcher: CoroutineDispatcher = Dispatchers.Main,
+//    private val action: suspend () -> Unit,
+//    private val after: Duration = Duration.ZERO,
+//) : CSFunc {
+//    companion object {
+//        fun CSHasRegistrations.throttler(
+//            after: Duration, function: suspend () -> Unit) =
+//            Throttler(this, Main, function, after)
+//
+//        fun CSHasRegistrations.throttler(
+//            dispatcher: CoroutineDispatcher = Main,
+//            after: Duration, function: suspend () -> Unit) =
+//            Throttler(this, dispatcher, function, after)
+//
+//        fun CSHasRegistrations.throttler(
+//            dispatcher: CoroutineDispatcher = Main, function: suspend () -> Unit) =
+//            Throttler(this, dispatcher, function)
+//
+//        fun CSHasRegistrations.throttler(
+//            function: suspend () -> Unit) =
+//            Throttler(this, Main, function)
+//    }
+//
+//    private val flow = MutableSharedFlow<Unit>(
+//        replay = 0, extraBufferCapacity = 1,
+//        onBufferOverflow = BufferOverflow.DROP_OLDEST
+//    )
+//
+//    init {
+//        parent.launch(dispatcher) {
+//            flow.collect {
+//                action()
+//                if (after > Duration.ZERO) delay(after)
+//            }
+//        }
+//    }
+//
+//    override operator fun invoke() {
+//        flow.tryEmit(Unit)
+//    }
+//}
 
 /**
  * 1. On the first call, [action] is executed after [after].
  * 2. Any calls arriving while waiting are ignored.
  */
-class Throttler(
+class Throttler<T>(
     parent: CSHasRegistrations,
     dispatcher: CoroutineDispatcher = Dispatchers.Main,
-    private val action: suspend () -> Unit,
+    private val action: suspend (T) -> Unit,
     private val after: Duration = Duration.ZERO,
-) : CSFunc {
+) : (T) -> Unit {  // Now the class itself is a function that takes T
     companion object {
         fun CSHasRegistrations.throttler(
             after: Duration, function: suspend () -> Unit) =
-            Throttler(this, Main, function, after)
+            Throttler<Unit>(this, Main, { function() }, after)
 
         fun CSHasRegistrations.throttler(
             dispatcher: CoroutineDispatcher = Main,
             after: Duration, function: suspend () -> Unit) =
-            Throttler(this, dispatcher, function, after)
+            Throttler<Unit>(this, dispatcher, { function() }, after)
 
         fun CSHasRegistrations.throttler(
             dispatcher: CoroutineDispatcher = Main, function: suspend () -> Unit) =
-            Throttler(this, dispatcher, function)
+            Throttler<Unit>(this, dispatcher, { function() })
 
         fun CSHasRegistrations.throttler(
             function: suspend () -> Unit) =
-            Throttler(this, Main, function)
+            Throttler<Unit>(this, Main, { function() })
+
+        fun <T> CSHasRegistrations.throttler(
+            after: Duration, function: suspend (T) -> Unit
+        ) = Throttler(this, Main, function, after)
+
+        fun <T> CSHasRegistrations.throttler(
+            dispatcher: CoroutineDispatcher = Main,
+            after: Duration, function: suspend (T) -> Unit
+        ) = Throttler(this, dispatcher, function, after)
+
+        fun <T> CSHasRegistrations.throttler(
+            dispatcher: CoroutineDispatcher = Main, function: suspend (T) -> Unit
+        ) = Throttler(this, dispatcher, function)
+
+        fun <T> CSHasRegistrations.throttler(
+            function: suspend (T) -> Unit
+        ) = Throttler(this, Main, function)
+
+        operator fun Throttler<Unit>.invoke() = invoke(Unit)
     }
 
-    private val flow = MutableSharedFlow<Unit>(
+    private val flow = MutableSharedFlow<T>(
         replay = 0, extraBufferCapacity = 1,
         onBufferOverflow = BufferOverflow.DROP_OLDEST
     )
 
     init {
         parent.launch(dispatcher) {
-            flow.collect {
-                action()
+            flow.collect { param ->
+                action(param)
                 if (after > Duration.ZERO) delay(after)
             }
         }
     }
 
-    override operator fun invoke() {
-        flow.tryEmit(Unit)
+    override operator fun invoke(param: T) {
+        flow.tryEmit(param)
     }
 }
