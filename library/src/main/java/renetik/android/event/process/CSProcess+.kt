5cea7a571bc2415d62da5b3094d084c55145afaf
Rene Dohan <dohan.rene@email.com>
2025-03-08 11:35:44 +0800

    Remove process and operation..

--- library/src/main/java/renetik/android/event/process/CSProcess+.kt
+++
@@ -1,7 +0,0 @@
-package renetik.android.event.process
-
-@Deprecated("In favor of coroutines", ReplaceWith("Coroutines"))
-fun <Data : Any, Process : CSProcess<Data>>
-        Process.connect(process: CSProcess<Data>) = apply {
-    onSuccess { process.success(it.data!!) }.onFailed { process.failed(it) }
-}
