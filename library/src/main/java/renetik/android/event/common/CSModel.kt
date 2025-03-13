d585d0d893f434511f9c8650672807b5c74a1d9f
Rene Dohan <dohan.rene@gmail.com>
2025-03-13 21:59:21 +0800

    ...

--- library/src/main/java/renetik/android/event/common/CSModel.kt
+++ library/src/main/java/renetik/android/event/common/CSModel.kt
@@ -1,13 +1,11 @@
 package renetik.android.event.common
 
-import androidx.annotation.AnyThread
 import renetik.android.core.kotlin.className
 import renetik.android.core.lang.CSLeakCanary.expectWeaklyReachable
 import renetik.android.core.lang.atomic.CSAtomic.Companion.atomic
 import renetik.android.core.logging.CSLog.logWarnTrace
 import renetik.android.event.CSEvent.Companion.event
 import renetik.android.event.invoke
-import renetik.android.event.registration.CSRegistration
 import renetik.android.event.registration.CSRegistrationsMap
 
 open class CSModel(
@@ -15,11 +13,6 @@ open class CSModel(
 ) : CSHasRegistrationsHasDestruct {
 
     final override val registrations by lazy { CSRegistrationsMap(this) }
-
-    @Synchronized
-    @AnyThread
-    fun register(key: String, registration: CSRegistration?): CSRegistration? =
-        registrations.register(key, registration)
 
     final override val eventDestruct = event<Unit>()
 
