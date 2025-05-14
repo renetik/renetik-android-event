6f31b2c9d54509d097d36c0c7a9c4c8c55d740a2
Rene Dohan <dohan.rene@gmail.com>
2025-05-14 17:11:56 +0800

    feat: Add isDestruct extension function
    Adds an extension function `isDestruct` to the `CSHasDestruct` interface. This function returns a `CSHasChangeValue<Boolean>` which can be used to observe changes in the `isDestructed` state.
    
    Also includes a TODO comment suggesting that the interface could potentially use just a `isDestructed: CSHasChangeValue<Boolean>` property directly.

--- library/src/main/java/renetik/android/event/common/CSHasDestruct.kt
+++ library/src/main/java/renetik/android/event/common/CSHasDestruct.kt
@@ -1,9 +1,15 @@
 package renetik.android.event.common
 
 import renetik.android.event.CSEvent
+import renetik.android.event.delegate
+import renetik.android.event.registration.CSHasChangeValue
 
+// TODO: should use just property isDestructed:CSHasChangeValue<Boolean>
 interface CSHasDestruct {
     val isDestructed: Boolean
     val eventDestruct: CSEvent<Unit>
     fun onDestruct()
 }
+
+fun CSHasDestruct.isDestruct(): CSHasChangeValue<Boolean> =
+    eventDestruct.delegate { isDestructed }
