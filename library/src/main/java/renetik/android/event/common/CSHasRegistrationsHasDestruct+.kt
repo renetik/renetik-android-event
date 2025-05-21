1d6a9b68b002d0d4509a398a277e2ef5098dbe9d
Rene Dohan <dohan.rene@gmail.com>
2025-05-21 13:38:28 +0800

    Refactor: Replace `registerListenOnce` with `listenOnce`
    This commit replaces the usage of `registerListenOnce` with the more direct `listenOnce` method for event listeners that should only fire once. This improves code clarity and conciseness.

--- library/src/main/java/renetik/android/event/common/CSHasRegistrationsHasDestruct+.kt
+++ library/src/main/java/renetik/android/event/common/CSHasRegistrationsHasDestruct+.kt
@@ -1,9 +1,11 @@
 package renetik.android.event.common
 
+import renetik.android.event.listenOnce
 import renetik.android.event.registration.CSHasRegistrations
-import renetik.android.event.registration.registerListenOnce
+import renetik.android.event.registration.plus
 
 fun <T> T.parent(parent: CSHasDestruct)
-    where  T : CSHasRegistrations, T : CSHasDestruct = apply {
-    registerListenOnce(parent.eventDestruct) { if (!isDestructed) destruct() }
+        where  T : CSHasRegistrations, T : CSHasDestruct = apply {
+    this + parent.eventDestruct.listenOnce { if (!isDestructed) destruct() }
+//    registerListenOnce(parent.eventDestruct) { if (!isDestructed) destruct() }
 }
--- library/src/main/java/renetik/android/event/registration/CSHasRegistrations+Event.kt
+++ library/src/main/java/renetik/android/event/registration/CSHasRegistrations+Event.kt
@@ -1,14 +1,11 @@
 package renetik.android.event.registration
 
-import androidx.annotation.UiThread
-import renetik.android.event.CSEvent
-
-inline fun <T> CSHasRegistrations.registerListenOnce(
-    event: CSEvent<T>, @UiThread crossinline listener: (argument: T) -> Unit
-): CSRegistration {
-    var registration: CSRegistration? = null
-    return (this + event.listen { argument ->
-        listener(argument)
-        registration?.cancel()
-    }).also { registration = it }
-}
+//inline fun <T> CSHasRegistrations.registerListenOnce(
+//    event: CSEvent<T>, @UiThread crossinline listener: (argument: T) -> Unit
+//): CSRegistration {
+//    var registration: CSRegistration? = null
+//    return (this + event.listen { argument ->
+//        listener(argument)
+//        registration?.cancel()
+//    }).also { registration = it }
+//}
