f6e0d97ebb58b5b4b0907738d528dd8881bb2dce
Rene Dohan <dohan.rene@gmail.com>
2025-08-16 11:52:20 +0800

    Test: Add CSModel parent/child destruct tests
    This commit adds unit tests for the `CSModel` class to verify the correct destruction behavior of parent and child models.
    
    - `testChildDestruct`: Verifies that when a child model is destructed, its registrations are cleared, and it is marked as destructed, while the parent model remains unaffected.
    - `testParentDestruct`: Verifies that when a parent model is destructed, both the parent and its child model have their registrations cleared and are marked as destructed.

---
+++ library/src/test/java/renetik/android/common/CSModelTest.kt
@@ -0,0 +1,42 @@
+package renetik.android.common
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import renetik.android.event.common.CSModel
+import renetik.android.event.common.destruct
+import renetik.android.event.common.onDestructed
+
+class CSModelTest {
+    @Test
+    fun testChildDestruct() {
+        val parent = CSModel()
+        assertEquals(0, parent.registrations.size)
+        val child = CSModel(parent)
+        var isDestructedCount = 0
+        child.onDestructed { isDestructedCount += 1 }
+        assertEquals(0, parent.registrations.size)
+        assertEquals(1, child.registrations.size)
+        child.destruct()
+        assertEquals(0, parent.registrations.size)
+        assertEquals(0, child.registrations.size)
+        assertEquals(true, child.isDestructed)
+        assertEquals(1, isDestructedCount)
+    }
+
+    @Test
+    fun testParentDestruct() {
+        val parent = CSModel()
+        assertEquals(0, parent.registrations.size)
+        val child = CSModel(parent)
+        var isDestructedCount = 0
+        child.onDestructed { isDestructedCount += 1 }
+        assertEquals(0, parent.registrations.size)
+        assertEquals(1, child.registrations.size)
+        parent.destruct()
+        assertEquals(0, parent.registrations.size)
+        assertEquals(0, child.registrations.size)
+        assertEquals(true, parent.isDestructed)
+        assertEquals(true, child.isDestructed)
+        assertEquals(1, isDestructedCount)
+    }
+}
