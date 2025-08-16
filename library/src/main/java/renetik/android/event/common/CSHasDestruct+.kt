c1b9953df00c6c731dd202adc38f43299596abf6
Rene Dohan <dohan.rene@gmail.com>
2025-08-16 12:02:32 +0800

    Refactor: Add onDestructed overload with parent registration
    This commit introduces a new overload for the `onDestructed` function in `CSHasDestruct+.kt`.
    
    The new overload accepts a `CSHasRegistrations` as a `parent` parameter. This allows the lifecycle of the `onDestructed` listener to be tied to the parent's registration lifecycle. The listener will be automatically cancelled when the parent is cancelled or when the `CSHasDestruct` instance is destructed, whichever comes first.
    
    - Added `onDestructed(parent: CSHasRegistrations, func: Func)` extension function to `CSHasDestruct`.
    - The listener (`func`) is invoked when the `CSHasDestruct` instance's `eventDestruct` is triggered.
    - The registration for this listener is added to the provided `parent` `CSHasRegistrations`.
    - The registration is cancelled either when the parent is cancelled or when the `eventDestruct` is triggered.

--- library/src/main/java/renetik/android/event/common/CSHasDestruct+.kt
+++ library/src/main/java/renetik/android/event/common/CSHasDestruct+.kt
@@ -4,12 +4,25 @@ import renetik.android.core.lang.Func
 import renetik.android.event.delegate
 import renetik.android.event.listen
 import renetik.android.event.registration.CSHasChangeValue
+import renetik.android.event.registration.CSHasRegistrations
 import renetik.android.event.registration.CSRegistration
+import renetik.android.event.registration.invoke
+import renetik.android.event.registration.plus
 
 fun <T : CSHasDestruct> T.destruct() = onDestruct()
 
 inline fun CSHasDestruct.onDestructed(crossinline func: Func): CSRegistration =
     eventDestruct.listen(func)
+
+inline fun CSHasDestruct.onDestructed(
+    parent: CSHasRegistrations, crossinline func: Func
+) {
+    var registration: CSRegistration? = null
+    registration = parent + eventDestruct.invoke {
+        registration?.cancel()
+        func()
+    }
+}
 
 val CSHasDestruct.isDestruct: CSHasChangeValue<Boolean>
     get() = eventDestruct.delegate { isDestructed }
