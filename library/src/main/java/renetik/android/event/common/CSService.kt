c3a754a8bf65f9cb0a223ec56c6fed54a7caab26
Rene Dohan <dohan.rene@gmail.com>
2025-10-15 14:06:34 +0800

    Refactor: CSServiceConnection
    Move CSServiceConnection to the event library and make it a generic class.
    This simplifies the implementation in `InstApplication` by removing boilerplate code for service connection handling.

--- library/src/main/java/renetik/android/event/common/CSService.kt
+++ library/src/main/java/renetik/android/event/common/CSService.kt
@@ -1,8 +1,11 @@
 package renetik.android.event.common
 
 import android.app.Service
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Binder
+import android.os.IBinder
 import renetik.android.core.kotlin.className
 import renetik.android.core.lang.CSLeakCanary.expectWeaklyReachable
 import renetik.android.core.lang.atomic.CSAtomic.Companion.atomic
@@ -11,6 +14,22 @@ import renetik.android.core.logging.CSLog.logWarnTrace
 import renetik.android.event.CSEvent.Companion.event
 import renetik.android.event.fire
 import renetik.android.event.registration.CSRegistrationsMap
+
+open class CSServiceConnection<Service : CSService> : ServiceConnection {
+    override fun onServiceConnected(name: ComponentName?, binder: IBinder?) {
+        @Suppress("UNCHECKED_CAST")
+        onConnected((binder as CSServiceBinder).service as Service)
+    }
+
+    override fun onServiceDisconnected(name: ComponentName?) = onDisconnected()
+
+    override fun onBindingDied(name: ComponentName?) = onDisconnected()
+
+    override fun onNullBinding(name: ComponentName?) = onDisconnected()
+
+    open fun onConnected(service: Service) = Unit
+    open fun onDisconnected() = Unit
+}
 
 class CSServiceBinder(service: CSService) : Binder() {
     val service: CSService? by weak(service)
