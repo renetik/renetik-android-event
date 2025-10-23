c821dee18ef615ec213179da16328d3d7c0fc535
Rene Dohan <dohan.rene@gmail.com>
2025-10-24 00:25:26 +0800

    Refactor: Move CSValue operator functions and add Float extensions
    This commit moves operator functions for `CSValue<Int>` (plus, minus, times, div, compareTo) from the `renetik-android-event` module to the `renetik-android-core` module. This refactoring improves code organization by centralizing core value object functionality.
    
    Additionally, new `rest` extension functions for `Float` and `CSValue<Float>` have been added to handle remainder operations with `Int` and `CSValue<Int>`.
    
    The affected files were updated to use the new import paths for these operator functions.

--- library/src/main/java/renetik/android/event/property/CSValue+Int.kt
+++ library/src/main/java/renetik/android/event/property/CSValue+Int.kt
@@ -15,11 +15,3 @@ fun <Item : CSHasDestruct> CSValue<Int>.update(
 ): Unit = list.size.update(value,
     onAdd = { index -> list.add(function(index)) },
     onRemove = { index -> list.removeAt(index).destruct() })
-
-operator fun CSValue<Int>.times(value: Int): Int = this.value * value
-operator fun CSValue<Int>.div(value: Int): Int = this.value / value
-operator fun CSValue<Int>.div(value: Float): Float = this.value / value
-operator fun CSValue<Int>.plus(value: Int): Int = this.value + value
-operator fun CSValue<Int>.minus(value: Int): Int = this.value - value
-operator fun CSValue<Int>.minus(value: CSValue<Int>): Int = this.value - value.value
-operator fun Int.minus(value: CSValue<Int>): Int = this - value.value
