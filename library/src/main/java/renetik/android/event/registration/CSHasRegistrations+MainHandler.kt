8cd91d1474cea7e2ecdea3805190fb2d5cece549
Rene Dohan <dohan.rene@gmail.com>
2025-11-02 18:21:20 +0100

    feat(event): Add `launchRepeat` overload for Main dispatcher
    This commit introduces a new overload for the `launchRepeat` function in `CSHasRegistrations`.
    This new function defaults to using the `Main` coroutine dispatcher, simplifying calls that need to run on the main thread.
    
    The existing `launchRepeat` function, which already defaulted to the `Main` dispatcher, is updated to call this new, more flexible overload.

--- library/src/main/java/renetik/android/event/registration/CSHasRegistrations+MainHandler.kt
+++ library/src/main/java/renetik/android/event/registration/CSHasRegistrations+MainHandler.kt
@@ -11,7 +11,7 @@ import kotlin.time.Duration
 
 @AnyThread
 inline fun CSHasRegistrations.laterEach(
-    delay: Int , after: Int = delay,
+    delay: Int, after: Int = delay,
     start: Boolean = true, crossinline function: () -> Unit,
 ): CSRegistration = this + mainHandler.laterEach(after, delay, start, function)
 
@@ -30,8 +30,15 @@ fun CSHasRegistrations.launchWhileActive(
 inline fun CSHasRegistrations.launchRepeat(
     delay: Duration, after: Duration = delay, start: Boolean = true,
     crossinline function: suspend () -> Unit,
+): CSRegistration = launchRepeat(Main, delay, after, start, function)
+
+@AnyThread
+inline fun CSHasRegistrations.launchRepeat(
+    dispatcher: CoroutineDispatcher = Main,
+    delay: Duration, after: Duration = delay, start: Boolean = true,
+    crossinline function: suspend () -> Unit,
 ): CSRegistration = launchRepeat(
-    dispatcher = Main, delay.inWholeMilliseconds.toInt(),
+    dispatcher, delay.inWholeMilliseconds.toInt(),
     after.inWholeMilliseconds.toInt(), start, function
 )
 
