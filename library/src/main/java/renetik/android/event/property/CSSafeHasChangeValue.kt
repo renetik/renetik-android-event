34442b6a1dd06ccd2a271a3dbd0e2220b49ea46b
Rene Dohan <dohan.rene@gmail.com>
2025-12-10 16:38:57 +0800

    feat: Add `safeValue` extension functions
    This commit introduces new extension functions to create a `CSSafeValue` from a `CSHasChangeValue`.
    
    - A `safeValue` extension for `CSHasRegistrations` is added, which takes a `CSHasChangeValue` property.
    - A `safeValue` extension for `CSHasChangeValue` is added, requiring a `CSHasRegistrations` parent to manage the lifecycle of the value subscription.

--- library/src/main/java/renetik/android/event/property/CSSafeHasChangeValue.kt
+++ library/src/main/java/renetik/android/event/property/CSSafeHasChangeValue.kt
@@ -4,6 +4,7 @@ import renetik.android.core.lang.ArgFun
 import renetik.android.core.lang.value.CSSafeValue
 import renetik.android.event.common.CSHasDestruct
 import renetik.android.event.registration.CSHasChangeValue
+import renetik.android.event.registration.CSHasRegistrations
 import renetik.android.event.registration.plus
 import renetik.android.event.util.CSLater.onMain
 import kotlin.reflect.KProperty
@@ -30,6 +31,22 @@ interface CSSafeHasChangeValue<T> : CSSafeValue<T>, CSHasChangeValue<T> {
                 override fun onValueChanged(newValue: T, fire: Boolean) {
                     isChanged = true
                     if (fire) onMain(::fireChange)
+                }
+            }
+        }
+
+        fun <T> CSHasRegistrations.safeValue(property: CSHasChangeValue<T>)
+                : CSSafeValue<T> = property.safeValue(this)
+
+        fun <T> CSHasChangeValue<T>.safeValue(
+            parent: CSHasRegistrations): CSSafeValue<T> = let { property ->
+            object : CSSafeValue<T> {
+                @Volatile
+                override var value: T = property.value
+                override fun getValue(thisRef: Any?, property: KProperty<*>): T = value
+
+                init {
+                    parent + property.onChange { value = it }
                 }
             }
         }
