6447a0bf3d83341981973159173fabb35eab769d
Rene Dohan <dohan.rene@gmail.com>
2026-01-09 22:49:48 +0800

    refactor: Rework `CSSafeHasChangeValue` implementation
    This commit refactors the internal implementation of the `safe` function in `CSSafeHasChangeValue`.
    
    The key changes include:
    - Replacing `@Volatile var` with `AtomicReference` for managing the property's value, improving thread safety.
    - Switching from the `CSPropertyBase` base class to `CSModel`.
    - Utilizing `AtomicReference.exchange` to atomically update the value and check if it has changed, simplifying the change detection logic.
    - An explicit `eventChange` event is now used for broadcasting changes.

--- library/src/main/java/renetik/android/event/property/CSSafeHasChangeValue.kt
+++ library/src/main/java/renetik/android/event/property/CSSafeHasChangeValue.kt
@@ -1,12 +1,18 @@
+@file:OptIn(ExperimentalAtomicApi::class)
+
 package renetik.android.event.property
 
 import renetik.android.core.lang.ArgFun
 import renetik.android.core.lang.value.CSSafeValue
+import renetik.android.event.CSEvent.Companion.event
 import renetik.android.event.common.CSHasDestruct
+import renetik.android.event.common.CSModel
 import renetik.android.event.registration.CSHasChangeValue
 import renetik.android.event.registration.CSHasRegistrations
 import renetik.android.event.registration.plus
 import renetik.android.event.util.CSLater.onMain
+import kotlin.concurrent.atomics.AtomicReference
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
 import kotlin.reflect.KProperty
 
 interface CSSafeHasChangeValue<T> : CSSafeValue<T>, CSHasChangeValue<T> {
@@ -19,18 +25,24 @@ interface CSSafeHasChangeValue<T> : CSSafeValue<T>, CSHasChangeValue<T> {
             parent: CSHasDestruct? = null,
             onChange: ArgFun<T>? = null
         ): CSSafeHasChangeValue<T> = let { property ->
-            object : CSPropertyBase<T>(parent, onChange), CSSafeHasChangeValue<T> {
-                @Volatile
-                override var value: T = property.value
+            object : CSModel(parent), CSSafeHasChangeValue<T> {
+                private val _value = AtomicReference(property.value)
+                val eventChange by lazy { event<T>() }
+
+                override var value: T
+                    get() = _value.load()
+                    set(value) = _value.store(value)
+
                 override fun getValue(thisRef: Any?, property: KProperty<*>): T = value
+                override fun onChange(function: (T) -> Unit) = eventChange.listen(function)
 
                 init {
-                    this + property.onChange { value(it) }
-                }
-
-                override fun onValueChanged(newValue: T, fire: Boolean) {
-                    isChanged = true
-                    if (fire) onMain(::fireChange)
+                    this + property.onChange { newValue ->
+                        if (newValue != _value.exchange(newValue)) onMain {
+                            onChange?.invoke(newValue)
+                            eventChange.fire(newValue)
+                        }
+                    }
                 }
             }
         }
