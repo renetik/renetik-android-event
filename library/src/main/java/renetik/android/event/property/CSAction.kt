4ed40c93f23590df18cfd7f9947d3a3b0c9f118c
Rene Dohan <dohan.rene@gmail.com>
2026-02-19 15:17:50 +0800

    Refactor `CSActionInterface` to use explicit `setTrue()` instead of `invoke()`
    * Remove the `invoke()` operator extension for `CSActionInterface` in `CSAction.kt`.
    * Update various controller panel button classes (Sampler, Piano, Sequence, Chord, Scale, Guitar, Fader, and Pad) to use `setTrue()` explicitly in button action callbacks.
    * Replace the `renetik.android.event.property.invoke` import with `renetik.android.core.lang.variable.setTrue` across affected controller files.

--- library/src/main/java/renetik/android/event/property/CSAction.kt
+++ library/src/main/java/renetik/android/event/property/CSAction.kt
@@ -5,6 +5,5 @@ import renetik.android.core.lang.variable.setTrue
 
 typealias CSActionInterface = CSProperty<Boolean>
 
-operator fun CSActionInterface.invoke() = setTrue()
 fun CSActionInterface.start() = setTrue()
 fun CSActionInterface.stop() = setFalse()
