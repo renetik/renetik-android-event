c9bc344f922864f8d8a70403bcad95c9b651da12
Rene Dohan <dohan.rene@gmail.com>
2026-02-19 15:08:05 +0800

    Refactor `CSAction` to simplify syntax and remove redundant property
    * Remove the redundant `isRunning` extension property in `CSActionInterface`, as it duplicated existing boolean property functionality.
    * Simplify `start()` and `stop()` functions by using expression body syntax.
    * Remove an unused import of `isTrue`.

--- library/src/main/java/renetik/android/event/property/CSAction.kt
+++ library/src/main/java/renetik/android/event/property/CSAction.kt
@@ -1,6 +1,5 @@
 package renetik.android.event.property
 
-import renetik.android.core.lang.value.isTrue
 import renetik.android.core.lang.variable.setFalse
 import renetik.android.core.lang.variable.setTrue
 import renetik.android.event.property.CSProperty.Companion.property
@@ -8,9 +7,8 @@ import renetik.android.event.property.CSProperty.Companion.property
 typealias CSActionInterface = CSProperty<Boolean>
 
 operator fun CSActionInterface.invoke() = setTrue()
-fun CSActionInterface.start() {setTrue()}
-fun CSActionInterface.stop() {setFalse()}
-val CSActionInterface.isRunning get() = isTrue
+fun CSActionInterface.start() = setTrue()
+fun CSActionInterface.stop() = setFalse()
 
 object CSAction {
     fun action(): CSActionInterface = property(false)
