aa38ffdc89843cdd32f0ad01222db87e1ca258e9
Rene Dohan <dohan.rene@gmail.com>
2026-02-19 15:23:14 +0800

    Refactor `CSAction` and clean up `AppPlayback+` imports
    * Remove `start()` and `stop()` extension methods from `CSActionInterface`. Users should now use `setTrue()` and `setFalse()` directly on the underlying `CSProperty<Boolean>`.
    * Remove the unused import of `renetik.android.event.property.stop` in `AppPlayback+.kt`.

--- library/src/main/java/renetik/android/event/property/CSAction.kt
+++ library/src/main/java/renetik/android/event/property/CSAction.kt
@@ -1,9 +1,3 @@
 package renetik.android.event.property
 
-import renetik.android.core.lang.variable.setFalse
-import renetik.android.core.lang.variable.setTrue
-
 typealias CSActionInterface = CSProperty<Boolean>
-
-fun CSActionInterface.start() = setTrue()
-fun CSActionInterface.stop() = setFalse()
