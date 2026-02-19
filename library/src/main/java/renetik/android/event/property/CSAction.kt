e018f539f9becae7f2e566a95dd95242078ec36d
Rene Dohan <dohan.rene@gmail.com>
2026-02-19 15:16:25 +0800

    Refactor: Remove `CSAction.action()` in favor of `CSProperty.property(false)`
    This commit simplifies the property management API by removing the redundant `CSAction.action()` factory method and updating all call sites to use `CSProperty.property(false)` directly.
    
    *   **renetik-android-event**:
        *   Remove the `CSAction` object and its `action()` helper method.
        *   Maintain `CSActionInterface` typealias and its extension functions (`invoke`, `start`, `stop`) for boolean properties.
    *   **renetik-apps-library**:
        *   Update multiple controller and mode classes (including `SplitController`, `SamplerController`, `ChordController`, `FaderController`, `MixAppMode`, `SequencerAppMode`, `SequenceController`, `GuitarController`, `ControllerKeyboard`, `ScaleController`, and `PadController`) to initialize action properties using `CSProperty.property(false)`.
        *   Clean up unused imports related to `CSAction`.
        *   Minor code formatting and standardization of property declarations.

--- library/src/main/java/renetik/android/event/property/CSAction.kt
+++ library/src/main/java/renetik/android/event/property/CSAction.kt
@@ -2,14 +2,9 @@ package renetik.android.event.property
 
 import renetik.android.core.lang.variable.setFalse
 import renetik.android.core.lang.variable.setTrue
-import renetik.android.event.property.CSProperty.Companion.property
 
 typealias CSActionInterface = CSProperty<Boolean>
 
 operator fun CSActionInterface.invoke() = setTrue()
 fun CSActionInterface.start() = setTrue()
 fun CSActionInterface.stop() = setFalse()
-
-object CSAction {
-    fun action(): CSActionInterface = property(false)
-}
