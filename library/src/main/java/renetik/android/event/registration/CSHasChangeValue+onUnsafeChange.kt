e9d7b3b03bc404a4897bdb70a21fc57e49a57d3f
Rene Dohan <dohan.rene@gmail.com>
2026-06-22 15:26:22 +0800

    Update `onUnsafeChange` extensions and add new overloads
    - Add `@JvmName` annotations to `onUnsafeChange` extension functions to prevent JVM signature conflicts.
    - Implement a new `onUnsafeChange` overload for `Pair` where both elements implement `CSSafeHasChangeValue`.
    - Reformat `synchronized` blocks and type parameters in `CSQuintuple` and `CSSixtuple` extensions for improved readability and consistent line wrapping.
    - Relocate and clean up existing `onUnsafeChange` implementations within the file.

--- library/src/main/java/renetik/android/event/registration/CSHasChangeValue+onUnsafeChange.kt
+++ library/src/main/java/renetik/android/event/registration/CSHasChangeValue+onUnsafeChange.kt
@@ -9,6 +9,7 @@ import renetik.android.core.lang.tuples.CSQuintuple
 import renetik.android.core.lang.tuples.CSSixtuple
 import renetik.android.event.property.CSSafeHasChangeValue
 
+@JvmName("onUnsafeChangeWithSafeSecond")
 fun <Argument1, Argument2, Item1, Item2> Pair<Item1, Item2>.onUnsafeChange(
     onUnsafeChange: (Argument1, Argument2) -> Unit,
 ): CSRegistration
@@ -203,76 +204,6 @@ fun <Argument1, Argument2, Argument3, Argument4, Argument5,
     return registrations
 }
 
-@JvmName("onUnsafeChangeWithSafeThirdAndFifth")
-fun <Argument1, Argument2, Argument3, Argument4, Argument5,
-        Item1, Item2, Item3, Item4, Item5>
-        CSQuintuple<Item1, Item2, Item3, Item4, Item5>.onUnsafeChange(
-    onUnsafeChange: (Argument1, Argument2, Argument3, Argument4, Argument5) -> Unit,
-): CSRegistration
-        where Item1 : CSHasChangeValue<Argument1>,
-              Item2 : CSHasChangeValue<Argument2>,
-              Item3 : CSSafeHasChangeValue<Argument3>,
-              Item4 : CSHasChangeValue<Argument4>,
-              Item5 : CSSafeHasChangeValue<Argument5> {
-    val registrations = CSRegistrationsMap(className)
-    val lock = Any()
-    var value1 = first.value
-    var value2 = second.value
-    var value3 = third.value
-    var value4 = fourth.value
-    var value5 = fifth.value
-    fun fireChange(values: CSQuintuple<Argument1, Argument2, Argument3, Argument4, Argument5>) {
-        if (registrations.isActive) onUnsafeChange(
-            values.first, values.second, values.third, values.fourth, values.fifth)
-    }
-    registrations.register(first.onChange { newValue ->
-        val values =
-            synchronized<Any, CSQuintuple<Argument1, Argument2, Argument3, Argument4, Argument5>>(
-                lock) {
-                value1 = newValue
-                CSQuintuple(value1, value2, value3, value4, value5)
-            }
-        fireChange(values)
-    })
-    registrations.register(second.onChange { newValue ->
-        val values =
-            synchronized<Any, CSQuintuple<Argument1, Argument2, Argument3, Argument4, Argument5>>(
-                lock) {
-                value2 = newValue
-                CSQuintuple(value1, value2, value3, value4, value5)
-            }
-        fireChange(values)
-    })
-    registrations.register(third.onUnsafeChange { newValue ->
-        val values =
-            synchronized<Any, CSQuintuple<Argument1, Argument2, Argument3, Argument4, Argument5>>(
-                lock) {
-                value3 = newValue
-                CSQuintuple(value1, value2, value3, value4, value5)
-            }
-        fireChange(values)
-    })
-    registrations.register(fourth.onChange { newValue ->
-        val values =
-            synchronized<Any, CSQuintuple<Argument1, Argument2, Argument3, Argument4, Argument5>>(
-                lock) {
-                value4 = newValue
-                CSQuintuple(value1, value2, value3, value4, value5)
-            }
-        fireChange(values)
-    })
-    registrations.register(fifth.onUnsafeChange { newValue ->
-        val values =
-            synchronized<Any, CSQuintuple<Argument1, Argument2, Argument3, Argument4, Argument5>>(
-                lock) {
-                value5 = newValue
-                CSQuintuple(value1, value2, value3, value4, value5)
-            }
-        fireChange(values)
-    })
-    return registrations
-}
-
 @JvmName("onUnsafeChangeWithSafeSixth")
 fun <Argument1, Argument2, Argument3, Argument4, Argument5, Argument6,
         Item1, Item2, Item3, Item4, Item5, Item6>
@@ -303,57 +234,146 @@ fun <Argument1, Argument2, Argument3, Argument4, Argument5, Argument6,
             values.fifth, values.sixth)
     }
     registrations.register(first.onChange { newValue ->
-        val values =
-            synchronized<Any, CSSixtuple<Argument1, Argument2, Argument3, Argument4, Argument5, Argument6>>(
-                lock) {
-                value1 = newValue
-                CSSixtuple(value1, value2, value3, value4, value5, value6)
-            }
+        val values = synchronized<Any, CSSixtuple<Argument1, Argument2,
+                Argument3, Argument4, Argument5, Argument6>>(lock) {
+            value1 = newValue
+            CSSixtuple(value1, value2, value3, value4, value5, value6)
+        }
         fireChange(values)
     })
     registrations.register(second.onChange { newValue ->
-        val values =
-            synchronized<Any, CSSixtuple<Argument1, Argument2, Argument3, Argument4, Argument5, Argument6>>(
-                lock) {
-                value2 = newValue
-                CSSixtuple(value1, value2, value3, value4, value5, value6)
-            }
+        val values = synchronized<Any, CSSixtuple<Argument1, Argument2,
+                Argument3, Argument4, Argument5, Argument6>>(lock) {
+            value2 = newValue
+            CSSixtuple(value1, value2, value3, value4, value5, value6)
+        }
         fireChange(values)
     })
     registrations.register(third.onChange { newValue ->
-        val values =
-            synchronized<Any, CSSixtuple<Argument1, Argument2, Argument3, Argument4, Argument5, Argument6>>(
-                lock) {
-                value3 = newValue
-                CSSixtuple(value1, value2, value3, value4, value5, value6)
-            }
+        val values = synchronized<Any, CSSixtuple<Argument1, Argument2,
+                Argument3, Argument4, Argument5, Argument6>>(lock) {
+            value3 = newValue
+            CSSixtuple(value1, value2, value3, value4, value5, value6)
+        }
         fireChange(values)
     })
     registrations.register(fourth.onChange { newValue ->
-        val values =
-            synchronized<Any, CSSixtuple<Argument1, Argument2, Argument3, Argument4, Argument5, Argument6>>(
-                lock) {
-                value4 = newValue
-                CSSixtuple(value1, value2, value3, value4, value5, value6)
-            }
+        val values = synchronized<Any, CSSixtuple<Argument1, Argument2,
+                Argument3, Argument4, Argument5, Argument6>>(lock) {
+            value4 = newValue
+            CSSixtuple(value1, value2, value3, value4, value5, value6)
+        }
         fireChange(values)
     })
     registrations.register(fifth.onChange { newValue ->
-        val values =
-            synchronized<Any, CSSixtuple<Argument1, Argument2, Argument3, Argument4, Argument5, Argument6>>(
-                lock) {
-                value5 = newValue
-                CSSixtuple(value1, value2, value3, value4, value5, value6)
-            }
+        val values = synchronized<Any, CSSixtuple<Argument1, Argument2,
+                Argument3, Argument4, Argument5, Argument6>>(lock) {
+            value5 = newValue
+            CSSixtuple(value1, value2, value3, value4, value5, value6)
+        }
         fireChange(values)
     })
     registrations.register(sixth.onUnsafeChange { newValue ->
-        val values =
-            synchronized<Any, CSSixtuple<Argument1, Argument2, Argument3, Argument4, Argument5, Argument6>>(
-                lock) {
-                value6 = newValue
-                CSSixtuple(value1, value2, value3, value4, value5, value6)
-            }
+        val values = synchronized<Any, CSSixtuple<Argument1, Argument2,
+                Argument3, Argument4, Argument5, Argument6>>(lock) {
+            value6 = newValue
+            CSSixtuple(value1, value2, value3, value4, value5, value6)
+        }
+        fireChange(values)
+    })
+    return registrations
+}
+
+@JvmName("onUnsafeChangeWithSafeBoth")
+fun <Argument1, Argument2, Item1, Item2> Pair<Item1, Item2>.onUnsafeChange(
+    onUnsafeChange: (Argument1, Argument2) -> Unit,
+): CSRegistration
+        where Item1 : CSSafeHasChangeValue<Argument1>,
+              Item2 : CSSafeHasChangeValue<Argument2> {
+    val registrations = CSRegistrationsMap(className)
+    val lock = Any()
+    var value1 = first.value
+    var value2 = second.value
+    fun fireChange(values: Pair<Argument1, Argument2>) {
+        if (registrations.isActive) onUnsafeChange(values.first, values.second)
+    }
+    registrations.register(first.onUnsafeChange { newValue ->
+        val values = synchronized<Any, Pair<Argument1, Argument2>>(lock) {
+            value1 = newValue
+            value1 to value2
+        }
+        fireChange(values)
+    })
+    registrations.register(second.onUnsafeChange { newValue ->
+        val values = synchronized<Any, Pair<Argument1, Argument2>>(lock) {
+            value2 = newValue
+            value1 to value2
+        }
+        fireChange(values)
+    })
+    return registrations
+}
+
+@JvmName("onUnsafeChangeWithSafeThirdAndFifth")
+fun <Argument1, Argument2, Argument3, Argument4, Argument5,
+        Item1, Item2, Item3, Item4, Item5>
+        CSQuintuple<Item1, Item2, Item3, Item4, Item5>.onUnsafeChange(
+    onUnsafeChange: (Argument1, Argument2, Argument3, Argument4, Argument5) -> Unit,
+): CSRegistration
+        where Item1 : CSHasChangeValue<Argument1>,
+              Item2 : CSHasChangeValue<Argument2>,
+              Item3 : CSSafeHasChangeValue<Argument3>,
+              Item4 : CSHasChangeValue<Argument4>,
+              Item5 : CSSafeHasChangeValue<Argument5> {
+    val registrations = CSRegistrationsMap(className)
+    val lock = Any()
+    var value1 = first.value
+    var value2 = second.value
+    var value3 = third.value
+    var value4 = fourth.value
+    var value5 = fifth.value
+    fun fireChange(values: CSQuintuple<Argument1, Argument2, Argument3, Argument4, Argument5>) {
+        if (registrations.isActive) onUnsafeChange(
+            values.first, values.second, values.third, values.fourth, values.fifth)
+    }
+    registrations.register(first.onChange { newValue ->
+        val values = synchronized<Any, CSQuintuple<Argument1, Argument2,
+                Argument3, Argument4, Argument5>>(lock) {
+            value1 = newValue
+            CSQuintuple(value1, value2, value3, value4, value5)
+        }
+        fireChange(values)
+    })
+    registrations.register(second.onChange { newValue ->
+        val values = synchronized<Any, CSQuintuple<Argument1, Argument2,
+                Argument3, Argument4, Argument5>>(lock) {
+            value2 = newValue
+            CSQuintuple(value1, value2, value3, value4, value5)
+        }
+        fireChange(values)
+    })
+    registrations.register(third.onUnsafeChange { newValue ->
+        val values = synchronized<Any, CSQuintuple<Argument1, Argument2,
+                Argument3, Argument4, Argument5>>(lock) {
+            value3 = newValue
+            CSQuintuple(value1, value2, value3, value4, value5)
+        }
+        fireChange(values)
+    })
+    registrations.register(fourth.onChange { newValue ->
+        val values = synchronized<Any, CSQuintuple<Argument1, Argument2,
+                Argument3, Argument4, Argument5>>(lock) {
+            value4 = newValue
+            CSQuintuple(value1, value2, value3, value4, value5)
+        }
+        fireChange(values)
+    })
+    registrations.register(fifth.onUnsafeChange { newValue ->
+        val values = synchronized<Any, CSQuintuple<Argument1, Argument2,
+                Argument3, Argument4, Argument5>>(lock) {
+            value5 = newValue
+            CSQuintuple(value1, value2, value3, value4, value5)
+        }
         fireChange(values)
     })
     return registrations
@@ -468,11 +488,12 @@ fun <Argument1, Argument2, Argument3, Argument4, Argument5, Argument6,
     var value5 = fifth.value
     var value6 = sixth.value
     fun fireChange(
-        values: CSSixtuple<Argument1, Argument2, Argument3, Argument4, Argument5, Argument6>
+        values: CSSixtuple<Argument1, Argument2, Argument3,
+                Argument4, Argument5, Argument6>
     ) {
         if (registrations.isActive) onUnsafeChange(
-            values.first, values.second, values.third, values.fourth,
-            values.fifth, values.sixth)
+            values.first, values.second, values.third,
+            values.fourth, values.fifth, values.sixth)
     }
     registrations.register(first.onChange { newValue ->
         val values =
@@ -530,3 +551,4 @@ fun <Argument1, Argument2, Argument3, Argument4, Argument5, Argument6,
     })
     return registrations
 }
+
