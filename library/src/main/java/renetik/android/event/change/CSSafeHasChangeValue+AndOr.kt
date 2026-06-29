19e512fbb03d9399fa5530b11308e0e2c790c771
Rene Dohan <dohan.rene@gmail.com>
2026-06-29 15:31:52 +0800

    Rename `CSSafeHasChange+AndOr.kt` to `CSSafeHasChangeValue+AndOr.kt`
    This commit renames the file to `CSSafeHasChangeValue+AndOr.kt` to maintain naming consistency and performs minor code cleanup by removing unused imports.
    
    - **File Rename**: Changed `CSSafeHasChange+AndOr.kt` to `CSSafeHasChangeValue+AndOr.kt`.
    - **Cleanup**: Removed unused imports for `renetik.android.event.dispatch.*` and the `CSRegistration` companion.

--- library/src/main/java/renetik/android/event/change/CSSafeHasChangeValue+AndOr.kt
+++ library/src/main/java/renetik/android/event/change/CSSafeHasChangeValue+AndOr.kt
@@ -2,9 +2,7 @@
 
 package renetik.android.event.change
 
-import renetik.android.event.dispatch.*
 import renetik.android.event.registration.*
-import renetik.android.event.registration.CSRegistration.Companion.CSRegistration
 
 import renetik.android.core.kotlin.primitives.isTrue
 import renetik.android.core.lang.value.ifTrue
